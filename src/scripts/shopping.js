5ec4868b20e9e23f139d707a077ef132760cbc1b
renaudbaussart <renaud.baussart@proton.me>
2024-05-31 13:57:19 +0200

    feat(shopping.js): add a delete button for the list

--- src/scripts/shopping.js
+++ src/scripts/shopping.js
@@ -4,11 +4,18 @@ function addItemList(){
 
     if(itemToAdd){
         let newItem = document.createElement('li');
-        newItem.textContent = ' - ' + itemToAdd;
+        newItem.innerHTML = ' - ' + itemToAdd + '  <button class="deletebutton">X</button>';
+
         document.getElementById('shoppingList').append(newItem);
         newItem.classList.add("font-sketch");
         newItem.classList.add("text-xl");
         newItem.classList.add("py-2");
+        
+        let deleteButton = newItem.querySelector('.deletebutton');
+        deleteButton.addEventListener('click', function() {
+            newItem.remove();
+        });
+
         document.getElementById('articleToAdd').value = '';
         return;
     }
